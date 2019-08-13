a8ba07c45f8cdad5b4ae8dd181238f26bba1b0b6
pohlinwei <linwei0909@hotmail.com>
2019-08-13 23:28:27 +0800

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -7,6 +7,30 @@ const Project= require('./project.js');
 // General 
 const isLandscape = () => window.innerWidth > window.innerHeight;
 const scrollTo = element => isLandscape() ? element.scrollIntoView(false) : element.scrollIntoView(true);
+
+// For navigation
+const dropdownMenu = document.getElementById('dropdown-menu');
+let isMenuOpen = false;
+const serveMenu = () => {
+    for (let e of toggle) {
+        e.classList.add('hidden');
+    }
+    dropdownMenu.classList.remove('hidden');
+    isMenuOpen = true;
+}
+const closeMenu = () => {
+    dropdownMenu.classList.add('hidden');
+    for (let e of toggle) {
+        e.classList.remove('hidden');
+    }
+    isMenuOpen = false;
+}
+const toCloseMenu = () => {
+    if (isMenuOpen && isLandscape()) {
+        closeMenu();
+    }
+}
+
 // For home page
 const alternate = () => blink.style.color = blink.style.color == 'rgb(255, 255, 255)' 
                         ? 'rgb(0, 0, 0)'
@@ -22,7 +46,7 @@ const showDescription = i => {
     currentDescription = allDescriptions[i];
     currentDescription.classList.remove('hidden');
     body.style.backgroundColor = '#EDEDED';
-    scrollTo(currentDescription);
+    currentDescription.scrollIntoView(true);
 }
 const hideDescription = () => { 
     // hide 'pop up' window
@@ -46,18 +70,31 @@ for (let i in files) {
 ====================*/
 // General
 const body = document.querySelector('body');
+body.onresize = toCloseMenu;
 
 // For navigation
 const allButtons = document.getElementsByClassName('button');
 const allContentDiv = document.getElementsByClassName('content');
 for (let i = 0; i < allButtons.length; i++) {
     const button = allButtons[i];
-    button.onclick = () => scrollTo(allContentDiv[i]);
+    button.onclick = () => {
+        if (i >= 3) {
+            closeMenu();
+        }
+        scrollTo(allContentDiv[i % 3]);
+    }
 }
 const homeButton = document.getElementById('home-button');
 const logo = document.getElementById('logo');
 homeButton.onclick = toHomeDiv;
 logo.onclick = toHomeDiv;
+const menuButton = document.getElementById('menu');
+menuButton.onclick = serveMenu;
+// Naviation in portrait mode
+const menuClose = document.getElementById('menu-close').children[0];
+const homeSmallButton = document.getElementById('home-small');
+menuClose.onclick = closeMenu;
+homeSmallButton.onclick = closeMenu;
 
 // For home page
 const homeDiv = document.getElementById('home-div');
